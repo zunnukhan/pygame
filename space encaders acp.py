f8dc0554a5c357cf80d81b1db7d22a69673aa2f4
zunnukhan <hajira_khan_786@yahoo.com>
2025-07-06 18:35:49 +0530

    4

--- space encaders acp.py
+++ space encaders acp.py
@@ -32,7 +32,7 @@ enemyX=[]
 enemyY=[]
 enemyX_change=[]
 enemyY_change=[]
-num_of_enemies=6
+num_of_enemies=7
 
 for _i in range(num_of_enemies):
     enemyImg.append(pygame.image.load("enemy.png"))
